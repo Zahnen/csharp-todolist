dfe05f2bfe00935376c78f09faac89f725eee3e3
Zahnen Garner <zahnen@gmail.com>
2021-01-03 18:18:03 -0800

    migrate schema for ToDoList

--- ToDoList/Models/ToDoListContext.cs
+++ ToDoList/Models/ToDoListContext.cs
@@ -6,12 +6,8 @@ namespace ToDoList.Models
   {
     public virtual DbSet<Category> Categories { get; set; }
     public DbSet<Item> Items { get; set; }
+    public DbSet<CategoryItem> CategoryItem { get; set; }
 
     public ToDoListContext(DbContextOptions options) : base(options) { }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-      optionsBuilder.UseLazyLoadingProxies();
-    }
   }
 }
